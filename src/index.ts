da84269f63c0652fa3482a8f99b4a4bfb0eebc9f
Joseph Pancho <31480952+janchpanch@users.noreply.github.com>
2026-06-16 14:26:36 -0500

    fetchFeed fully type checked and validated after a back-and-forth with boots. TypeScript sucks

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,39 @@
 import {
-  CommandsRegistry,
-  handlerLogin,
-  handlerRegister,
-  handlerReset,
-  handlerSandbox,
-  handlerUsers,
-  registerCommand,
-  runCommand,
+    CommandsRegistry,
+    handlerLogin,
+    handlerRegister,
+    handlerReset,
+    handlerSandbox,
+    handlerUsers,
+    registerCommand,
+    runCommand,
 } from "./commands";
 
 async function main() {
-  let reg: CommandsRegistry = {};
+    let reg: CommandsRegistry = {};
 
-  registerCommand(reg, "login", handlerLogin);
-  registerCommand(reg, "register", handlerRegister);
-  registerCommand(reg, "reset", handlerReset);
-  registerCommand(reg, "users", handlerUsers);
+    registerCommand(reg, "login", handlerLogin);
+    registerCommand(reg, "register", handlerRegister);
+    registerCommand(reg, "reset", handlerReset);
+    registerCommand(reg, "users", handlerUsers);
 
-  registerCommand(reg, "sandbox", handlerSandbox)
+    registerCommand(reg, "sandbox", handlerSandbox)
 
-  let input = process.argv;
-  let cmd = input[2];
-  let args = input.slice(3);
+    let input = process.argv;
+    let cmd = input[2];
+    let args = input.slice(3);
 
-  // console.log(input);
-  // console.log(cmd);
-  // console.log(args);
+    // console.log(input);
+    // console.log(cmd);
+    // console.log(args);
 
-  if (!cmd || cmd.length === 0) {
-    throw new Error("not enough arguments were provided");
-  }
+    if (!cmd || cmd.length === 0) {
+        throw new Error("not enough arguments were provided");
+    }
 
-  await runCommand(reg, cmd, ...args);
+    await runCommand(reg, cmd, ...args);
 
-  process.exit(0);
+    process.exit(0);
 }
 
 main();
--- src/lib/rss/rss.ts
+++ src/lib/rss/rss.ts
@@ -1,22 +1,122 @@
 import { XMLParser } from "fast-xml-parser";
+import { isRecord } from "../util/validation";
 
 type RSSFeed = {
-  channel: {
-    title: string;
-    link: string;
-    description: string;
-    item: RSSItem[];
-  };
+    channel: {
+        title: string;
+        link: string;
+        description: string;
+        item: RSSItem[];
+    };
 };
 
 type RSSItem = {
-  title: string;
-  link: string;
-  description: string;
-  pubDate: string;
+    title: string;
+    link: string;
+    description: string;
+    pubDate: string;
 };
 
 export async function fetchFeed(feedURL: string) {
-  let data = new Request(feedURL);
-  console.log(data);
+    // Assign the response obj as *response*, not any
+    let response: Response;
+    try {
+        response = await fetch(feedURL, {
+            headers: {
+                "User-Agent": "gator",
+            },
+        });
+    } catch (error) {
+        throw new Error(`Something went wrong: ${error}`);
+    }
+
+    let data = await response.text();
+    let parser = new XMLParser({
+        processEntities: false,
+    });
+
+    const parsed = parser.parse(data);
+
+    if (!isRecord(parsed)) {
+        throw new Error("invalid RSS feed");
+    }
+
+    const rss = parsed.rss;
+
+    if (!isRecord(rss)) {
+        throw new Error("invalid RSS feed");
+    }
+
+    const channel = rss.channel;
+
+    if (!isRecord(channel)) {
+        throw new Error("invalid RSS feed");
+    }
+
+    const { title, link, description } = channel;
+
+    if (
+        typeof title !== "string" ||
+        typeof link !== "string" ||
+        typeof description !== "string"
+    ) {
+        throw new Error("invalid RSS feed metadata");
+    }
+
+    // Handle the the RSSItem objects
+    const rawItem = channel.item;
+    let rawItems: unknown[];
+    let validItems: RSSItem[] = [];
+
+    console.log(typeof rawItem);
+
+
+    if (Array.isArray(rawItem)) {
+        rawItems = rawItem;
+        // console.log("Array check")
+        // console.log(rawItems)
+    } else if (isRecord(rawItem)) {
+        rawItems = [rawItem];
+        // console.log("Record check")
+    } else {
+        rawItems = [];
+        // console.log("Empty default")
+    }
+
+    // **IN** gives indices | **OF** yields objects. Python spoilage.
+    for (const rawItem of rawItems) {
+        if (!isRecord(rawItem)) {
+            // console.log("invalid record")
+            continue;
+        }
+
+        const { title, link, description, pubDate } = rawItem;
+
+        if (
+            typeof title !== "string" ||
+            typeof link !== "string" ||
+            typeof description !== "string" ||
+            typeof pubDate !== "string"
+        ) {
+            continue;
+        }
+
+        validItems.push({
+            title,
+            link,
+            description,
+            pubDate,
+        });
+
+    }
+
+    let feed: RSSFeed = {
+        channel: {
+            title: title,
+            link: link,
+            description: description,
+            item: validItems,
+        },
+    };
+    return feed;
 }
